5a074597a1019a1d2fed843f5e63be74d64fcc77
Erik <oieeri26@uw.edu>
2019-07-19 12:41:23 -0700

    Add courses with students

--- api/models/course.js
+++ api/models/course.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
+const studentSchema = require('./student')
 
-// schema
 const schema = new mongoose.Schema({
     name: String,
     start_date: Date,
     end_date: Date,
+    students: [studentSchema]
 }, {
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at'}
 })
 
-// export a model
 module.exports = mongoose.model('Course', schema)
--- api/routes/courses.js
+++ api/routes/courses.js
@@ -2,12 +2,26 @@ const router = require('express').Router()
 const Course = require('../models/course')
 
 // GET ALL 
-
 router.get('/', (req, res, next) => {
     const status = 200
     Course.find().then(response => {
         res.json({ status, response })
     })
 })
+
+// CREATE
+router.post('/', (req, res, next) => {
+    const status = 201
+    Course.create(req.body).then(response => {
+        throw 'everything is broken'
+        res.status(201).json({ status, response })
+    }).catch(error => {
+        console.error(error)
+        const err = new Error('Validation problem. Add a useful message')
+        err.status = 400
+        next(err)
+    })
+})
+
 
 module.exports = router
