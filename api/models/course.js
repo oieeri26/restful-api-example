5a29448f2281691a795f2a64183853d5fb84415f
Erik <oieeri26@uw.edu>
2019-07-19 11:03:04 -0700

    Create first route for course

---
+++ api/models/course.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+// schema
+const schema = new mongoose.Schema({
+    name: String,
+    start_date: Date,
+    end_date: Date,
+}, {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at'}
+})
+
+// export a model
+module.exports = mongoose.model('Course', schema)
